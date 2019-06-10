0541e64407794fa504b874c5cdbbd63cf935c5a4
Arunesh Mishra <aruneshm@gmail.com>
2019-06-09 20:41:45 -0700

    summon

--- play/background.js
+++ play/background.js
@@ -9,5 +9,11 @@ chrome.runtime.onInstalled.addListener(function() {
 
     chrome.storage.sync.set({color: '#3aa757'}, function() {
         console.log("The color is green.");
-    });
-});
+	});
+
+       });
+
+ chrome.browserAction.onClicked.addListener(function (tab) {
+		    alert("You summmoned me !");
+		    });
+
--- play/content.js
+++ play/content.js
@@ -2,3 +2,32 @@
 
 var firstHref = $("a[href^='http']").eq(0).attr("href");
 console.log(firstHref);
+$( document ).ready(function() {
+		$("a[href^='http']").each(function(index) {
+				console.log( index + ": " + $( this ).attr("href"));
+				});
+		doDialog();
+		});
+
+var div = document.createElement('div');
+document.body.appendChild(div);
+div.className = 'play-chatbot';
+div.setAttribute("style", "color:red; border: 1px solid blue;");
+
+function doDialog() {
+	$( "#dialog" ).dialog({
+		dialogClass: "no-close",
+		buttons: [{ text: "OK",
+			click: function() {
+				$( this ).dialog( "close" );
+			}
+		}
+		]
+	});
+}
+
+function browserActionCallback(tab ) {
+	doDialog();
+}
+
+
